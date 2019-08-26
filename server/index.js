ffc652920a83f51a36a8be6a68c0b4d41b41774d
usmanghulam <you@example.com>
2019-08-26 01:40:36 -0700

    adadf

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 app.use(express.static(path.resolve("./dist")))
+app.use(cors())
 const PORT = process.env.PORT || 3002
 app.listen(PORT, ()=> console.log("localhost//:3002"))
